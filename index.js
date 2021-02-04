5ce575f787f1c55e7552c0ac88243f3b59318947
kmundil <kelliejmundil@gmail.com>
2021-02-04 22:27:51 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// Welcome, new coder. Enter your code on line 2 to print 'Hello, World!' to the console (the panel on the right)
+console.log('Hello, World!') 
+// comments //
+
+
+/* Multiple lines
+of comments */
+
+// Next, PLAY! Modify your code on line 2 to try to accomplish the tasks listed in the "Your First Program" page.
